6de001c76025c58d7f750bbbccc6670bb84f8c5d
adhityasukma <sukmaadhitya@gmail.com>
2022-08-08 04:48:07 +0700

    first commit

---
+++ assets/js/myscript.js
@@ -0,0 +1,17 @@
+function gotoHome(){
+    const element = document.getElementById("home");
+    element.scrollIntoView();
+}
+function gotoAbout(){
+    const element = document.getElementById("about");
+    element.scrollIntoView();
+}
+function gotoProject(){
+    const element = document.getElementById("projects");
+    element.scrollIntoView();
+}
+function gotoContact(){
+    const element = document.getElementById("contact");
+    document.getElementById("name").focus();
+    element.scrollIntoView();
+}
