02f213328281be4106f8ab098c4fc0f107608da9
adhitya <sukmaadhitya@gmail.com>
2025-11-21 17:02:49 +0700

    Update portfolio (Skills & Project Portfolio)

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,17 +1,25 @@
-function gotoHome(){
-    const element = document.getElementById("home");
-    element.scrollIntoView();
+function gotoHome() {
+  const element = document.getElementById('home');
+  element.scrollIntoView();
 }
-function gotoAbout(){
-    const element = document.getElementById("about");
-    element.scrollIntoView();
+function gotoAbout() {
+  const element = document.getElementById('about');
+  element.scrollIntoView();
 }
-function gotoProject(){
-    const element = document.getElementById("projects");
-    element.scrollIntoView();
+function gotoSkill() {
+  const element = document.getElementById('skill');
+  element.scrollIntoView();
 }
-function gotoContact(){
-    const element = document.getElementById("contact");
-    document.getElementById("name").focus();
-    element.scrollIntoView();
+function gotoProject() {
+  const element = document.getElementById('projects');
+  element.scrollIntoView();
+}
+function gotoPortfolio() {
+  const element = document.getElementById('portfolio');
+  element.scrollIntoView();
+}
+function gotoContact() {
+  const element = document.getElementById('contact');
+  document.getElementById('name').focus();
+  element.scrollIntoView();
 }
